4b6b04c2daacacb8e1c21cabb5b5bfcd52232ad0
Monika <100003452+TurorialFnf@users.noreply.github.com>
2022-03-16 19:48:04 +0700

    Fix

--- source/states/FlashingLightsMenu.hx
+++ source/states/FlashingLightsMenu.hx
@@ -25,14 +25,31 @@ class FlashingLightsMenu extends MusicBeatState
 
     override function update(elapsed:Float)
     {
-        super.update(elapsed);
-
-        if(FlxG.keys.justPressed.Y)
-            Options.setData(true, "flashingLights");
-        else if(!FlxG.keys.justPressed.Y && FlxG.keys.justPressed.ANY)
-            Options.setData(false, "flashingLights");
-
-        if(FlxG.keys.justPressed.ANY)
-            FlxG.switchState(new TitleState());
-    }
+                      if(!leftState) {
+			var back:Bool = controls.BACK;
+			if (controls.ACCEPT || back) {
+				leftState = true;
+				FlxTransitionableState.skipNextTransIn = true;
+				FlxTransitionableState.skipNextTransOut = true;
+				if(!back) {
+					ClientPrefs.flashing = false;
+					ClientPrefs.saveSettings();
+					FlxG.sound.play(Paths.sound('confirmMenu'));
+					FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker) {
+						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
+							MusicBeatState.switchState(new TitleState());
+						});
+					});
+				} else {
+					FlxG.sound.play(Paths.sound('cancelMenu'));
+					FlxTween.tween(warnText, {alpha: 0}, 1, {
+						onComplete: function (twn:FlxTween) {
+							MusicBeatState.switchState(new TitleState());
+						}
+					});
+				}
+			}
+		}
+		super.update(elapsed);
+	}
 }
