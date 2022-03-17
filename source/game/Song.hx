0aee96868b8dd859addf22199871cf1e4a2bd713
BoyfriendlolNewAccount <101530288+BoyfriendlolNewAccount@users.noreply.github.com>
2022-03-17 08:29:06 -0300

    Add files via upload

--- source/game/Song.hx
+++ source/game/Song.hx
@@ -96,7 +96,7 @@ class Song
 
 		var rawJson:String = "";
 
-		rawJson = Assets.getText(Paths.json(folder.toLowerCase() + jsonInput.toLowerCase())).trim();
+		rawJson = SUtil.getPath() + Assets.getText(Paths.json(folder.toLowerCase() + jsonInput.toLowerCase())).trim();
 
 		if(rawJson != "")
 		{
