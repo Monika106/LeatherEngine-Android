bf4ff5cddc5cce3cb7ff63b627132ba8ad8cafd1
Monika <100003452+TurorialFnf@users.noreply.github.com>
2022-03-17 12:15:50 +0700

    Super idol

--- source/utilities/Controls.hx
+++ source/utilities/Controls.hx
@@ -314,7 +314,7 @@ class Controls extends FlxActionSet
 				inline forEachBound(Control.DOWN, (action, state) -> addbutton(action, virtualPad.buttonDown, state));
 				inline forEachBound(Control.LEFT, (action, state) -> addbutton(action, virtualPad.buttonLeft, state));
 				inline forEachBound(Control.RIGHT, (action, state) -> addbutton(action, virtualPad.buttonRight, state));	
-			case DUO:
+			case DPaD:
 				inline forEachBound(Control.UP, (action, state) -> addbutton(action, virtualPad.buttonUp, state));
 				inline forEachBound(Control.DOWN, (action, state) -> addbutton(action, virtualPad.buttonDown, state));
 				inline forEachBound(Control.LEFT, (action, state) -> addbutton(action, virtualPad.buttonLeft, state));
