be94639c58c974dfef6b70eccf8a678e47dafce3
RaidenAlfares12 <97826418+RaidenAlfares12@users.noreply.github.com>
2022-03-17 19:04:52 +0700

    WHY FC?!?!

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -121,7 +121,7 @@ class MainMenuState extends MusicBeatState
 		changeItem();
 
                 #if android
-                addVirtualPad(UP_DOWN, A_B);
+                addVirtualPad(UP_DOWN, A_B_C);
                 #end
 
 		super.create();
