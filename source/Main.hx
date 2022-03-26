6fe5454568dd6eecf98dfff7521d140a63b627be
Raiden Alfares 100% REAL <97826418+RaidenAlfares12@users.noreply.github.com>
2022-03-26 17:05:38 +0700

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -19,7 +19,7 @@ class Main extends Sprite
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-	var framerate:Int = 60; // How many frames per second the game should run at.
+	var framerate:Int = 120; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 
@@ -33,8 +33,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-
- 	        SUtil.gameCrashCheck();
 
 		if (stage != null)
 		{
@@ -70,12 +68,20 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		SUtil.doTheCheck();
+		#if !cpp
+		framerate = 60;
+		#end
+
+		#if !debug
+		initialState = TitleState;
+		#end
 
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
+		#if !mobile
 		display = new SimpleInfoDisplay(10, 3, 0xFFFFFF, "_sans");
 		addChild(display);
+		#end
 	}
 
 	public static var display:SimpleInfoDisplay;
