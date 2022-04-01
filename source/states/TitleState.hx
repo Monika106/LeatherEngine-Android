297c7014b574e7f65c306a5866ab9a2b1e557460
Heropowerbrine <78842166+Heropowerbrine@users.noreply.github.com>
2022-04-01 17:49:23 +0800

    No you're not

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -354,7 +354,7 @@ class TitleState extends MusicBeatState
 
 			new FlxTimer().start(2, function(tmr:FlxTimer)
 			{
-				MusicBeatState.switchState(new MainMenuState());
+				FlxG.switchState(new MainMenuState());
 			});
 
 			// FlxG.sound.play(Paths.music('titleShoot'), 0.7);
