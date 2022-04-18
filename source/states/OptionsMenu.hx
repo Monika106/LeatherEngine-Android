a692047b88a9ecd141a24115add03c4d41a5a6fe
BoyfriendlolNewAccount <101530288+BoyfriendlolNewAccount@users.noreply.github.com>
2022-04-18 09:26:16 -0300

    Ok, no ._.

--- source/states/OptionsMenu.hx
+++ source/states/OptionsMenu.hx
@@ -250,12 +250,6 @@ class OptionsMenu extends MusicBeatState
 
 		if (!inMenu)
 		{
-                 if (controls.ACCEPT) {
-			openSelectedSubstate(options[curSelected]);
-		}
-	}
-}
-
 			if(-1 * Math.floor(FlxG.mouse.wheel) != 0)
 			{
 				curSelected -= 1 * Math.floor(FlxG.mouse.wheel);
